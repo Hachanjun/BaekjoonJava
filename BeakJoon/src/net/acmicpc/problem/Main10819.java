75f8afe4ecf57244090096d89c9a31f47569ea84
ChanJunHa <hachanzzzz@gmail.com>
2023-02-02 22:53:13 +0900

    순열

--- BeakJoon/src/net/acmicpc/problem/Main10819.java
+++ BeakJoon/src/net/acmicpc/problem/Main10819.java
@@ -2,44 +2,57 @@ package net.acmicpc.problem;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
-// 이전 순열
-// 다음 순열에서 부등호만 바꾸면 된다.
-public class Main10973 {
+// 차이를 최대로
+/*
+ * 수 N개가 주어졌을 때, 식의 결과를 최대로 하는 결과 
+ * 제한이 8밖에 안 되며, 수의 순서만 변경할 수 있다.
+ * 제한이 8이기 때문에 8 * 8! = 32만 -> 모든 경우를 다 해봐도 된다.
+ */
+public class Main10819 {
 
 	public static void main(String[] args) throws Exception {
 		
 		// 입력
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		StringBuilder sb = new StringBuilder();
-		
-		// N이 주어진다.
+		// 3 <= N <= 8 인 N이 주어진다.
 		int N = Integer.parseInt(br.readLine());
 		
-		// 순열이 주어진다.
+		// 배열에 들어있는 정수가 주어진다.
 		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 		int[] permutation = new int[N];
 		for ( int i = 0; i < N; i++ ) {
 			permutation[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		if ( next_permutation(permutation) ) {
-			for ( int i = 0; i < N; i++ ) {
-				sb.append(permutation[i]).append(" ");
-			}
-			sb.append('\n');
-		} else {
-			sb.append("-1");
+		// 정렬은 필수 
+		// 다음 순열을 이용해서 모든 순열을 구하려면 모든 순열은 첫 순열 부터 시작해야한다.
+		// 첫 순열은 오름차순이니까 정렬을 해줘야한다.
+		Arrays.sort(permutation);
+		int ans = 0;
+		do {
+			int temp = calculator(permutation);
+			ans = Math.max(ans, temp);
+		} while( next_permutation(permutation) );
+		
+		System.out.println(ans);
+	}
+	
+	// 문제에 나와있는 식, 계속 인접한 것의 차이를 구한 후 절댓값을 구해준다.
+	public static int calculator(int[] permutation) {
+		int sum = 0;
+		for ( int i = 1; i < permutation.length; i++ ) {
+			sum += Math.abs(permutation[i] - permutation[i - 1]);
 		}
-		System.out.println(sb);
+		return sum;
 	}
 	
 	public static boolean next_permutation(int[] permutation) {
 		int standard = permutation.length - 1;
-		
-		while ( standard > 0 && permutation[standard - 1] <= permutation[standard] ) {
+		while ( standard > 0 && permutation[standard - 1] >= permutation[standard] ) {
 			standard -= 1;
 		}
 		
@@ -48,7 +61,7 @@ public class Main10973 {
 		}
 		
 		int prev = permutation.length - 1;
-		while ( permutation[prev] >= permutation[standard - 1] ) {
+		while ( permutation[prev] <= permutation[standard - 1] ) {
 			prev -= 1;
 		}
 		
@@ -57,7 +70,6 @@ public class Main10973 {
 		permutation[prev] = temp;
 		
 		prev = permutation.length - 1;
-		
 		while ( standard < prev ) {
 			temp = permutation[standard];
 			permutation[standard] = permutation[prev];
@@ -66,7 +78,6 @@ public class Main10973 {
 			standard += 1;
 			prev -= 1;
 		}
-		
 		return true;
 	}
 }
--- BeakJoon/src/net/acmicpc/problem/Main10974.java
+++ BeakJoon/src/net/acmicpc/problem/Main10974.java
@@ -2,15 +2,20 @@ package net.acmicpc.problem;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
-// 이전 순열
-// 다음 순열에서 부등호만 바꾸면 된다.
-public class Main10973 {
+// 모든 순열 
+/*
+ * do ~ while 을 많이 사용한다.
+ * 배열 a에 1~N까지의 수를 다 넣어주고 계속 순열을 출력하면서 다음 순열을 구해준다.
+ * 
+ * while에 대한 검사를 처음에 하면 다음 순열을 구한 이후에 모든 순열을 구하기 때문에 
+ * 첫 순열을 구할 수 없다.
+ */
+public class Main10974 {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception { 
 		
-		// 입력
+		// 입력 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		StringBuilder sb = new StringBuilder();
@@ -18,28 +23,26 @@ public class Main10973 {
 		// N이 주어진다.
 		int N = Integer.parseInt(br.readLine());
 		
-		// 순열이 주어진다.
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		// 배열에 1 ~ N까지의 수를 다 넣어준다.
 		int[] permutation = new int[N];
 		for ( int i = 0; i < N; i++ ) {
-			permutation[i] = Integer.parseInt(st.nextToken());
+			permutation[i] = i + 1;
 		}
 		
-		if ( next_permutation(permutation) ) {
+		do {
 			for ( int i = 0; i < N; i++ ) {
 				sb.append(permutation[i]).append(" ");
 			}
 			sb.append('\n');
-		} else {
-			sb.append("-1");
-		}
+		} while (next_purmutation(permutation));
+		
 		System.out.println(sb);
 	}
 	
-	public static boolean next_permutation(int[] permutation) {
-		int standard = permutation.length - 1;
+	public static boolean next_purmutation(int[] permutation) {
 		
-		while ( standard > 0 && permutation[standard - 1] <= permutation[standard] ) {
+		int standard = permutation.length - 1;
+		while ( standard > 0 && permutation[standard - 1] >= permutation[standard] ) {
 			standard -= 1;
 		}
 		
@@ -48,7 +51,7 @@ public class Main10973 {
 		}
 		
 		int prev = permutation.length - 1;
-		while ( permutation[prev] >= permutation[standard - 1] ) {
+		while ( permutation[prev] <= permutation[standard - 1] ) {
 			prev -= 1;
 		}
 		
@@ -66,7 +69,6 @@ public class Main10973 {
 			standard += 1;
 			prev -= 1;
 		}
-		
 		return true;
 	}
 }
